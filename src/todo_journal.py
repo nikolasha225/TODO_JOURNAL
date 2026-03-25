01ad5784c761b7d3586cce7e5498ff9f665de62a
Ducktor74 <nikolasha225@yandex.ru>
2026-03-25 23:15:58 +0300

    Снова настроил poerty

--- src/todo_journal.py
+++ src/todo_journal.py
@@ -1,17 +1,15 @@
-"""Модуль для работы с todo-журналом."""
-
+#Модуль для работы с журналом
 import json
 import sys
 from typing import Dict, List, Union, NoReturn
 
 
+# Базовое исключение для журнала (мб пригодится)
 class TodoJournalError(Exception):
-    """Базовое исключение для TodoJournal."""
     pass
 
-
+#==============Класс для управления журналом===================
 class TodoJournal:
-    """Класс для управления todo-журналом."""
 
     def __init__(self, path_todo: str):
         self.path_todo = path_todo
@@ -19,8 +17,8 @@ class TodoJournal:
         self.name: str = data["name"]
         self.entries: List[str] = data["todos"]
 
+    # Чтение и парсинг JSON
     def _parse(self) -> Dict[str, Union[str, List[str]]]:
-        """Чтение и парсинг JSON-файла."""
         try:
             with open(self.path_todo, 'r', encoding='utf-8') as f:
                 data = json.load(f)
@@ -28,23 +26,23 @@ class TodoJournal:
         except FileNotFoundError:
             sys.exit(1)
 
+    # Запись данных в файл
     def _update(self, new_data: Dict[str, Union[str, List[str]]]) -> None:
-        """Запись данных в файл."""
         with open(self.path_todo, 'w', encoding='utf-8') as f:
             json.dump(new_data, f, sort_keys=True, indent=4, ensure_ascii=False)
 
+    #Создание нового пустого журнала (единоразовый метод)
     @staticmethod
     def create(filename: str, name: str) -> None:
-        """Создание нового пустого журнала."""
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump({'name': name, 'todos': []}, f, sort_keys=True, indent=4, ensure_ascii=False)
 
+    # Добавление задачи
     def add_entry(self, new_entry: str) -> None:
-        """Добавление задачи."""
         self.entries.append(new_entry)
         self._update({'name': self.name, 'todos': self.entries})
 
+    #Удаление задачи по индексу
     def remove_entry(self, index: int) -> None:
-        """Удаление задачи по индексу."""
         del self.entries[index]
         self._update({'name': self.name, 'todos': self.entries})
